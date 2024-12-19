016a3d7b776c8fa47da0fbcce150dfca0e76dfb1
Libing Chen <libing.chen@gmail.com>
2024-12-19 10:43:26 +0800

    chore: removed

--- word_suffix/__init__.py
+++
@@ -1,5 +0,0 @@
-from word_suffix import word_suffix_stub
-
-
-def find_words(words, suffix):
-    return word_suffix_stub.find_words(words, suffix)
--- word_suffix/demo.py
+++
@@ -1,3 +0,0 @@
-from word_suffix import find_words
-
-print(find_words("Baz,Jazz,Mash,Splash,Squash", "sh"))
